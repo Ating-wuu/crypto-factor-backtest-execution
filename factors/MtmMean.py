b353a4cd1e990f7199f6cef8c56a7f5a6f3de4d4
Ating-wuu <wuyating199711@gmail.com>
2025-11-25 17:22:04 +0800

    Create MtmMean.py

---
+++ factors/MtmMean.py
@@ -0,0 +1,10 @@
+def signal(*args):
+    df = args[0]
+    n = args[1]
+    factor_name = args[2]
+
+    df['MtmMean'] = df['close'].pct_change(n).rolling(n).mean()
+    df['std'] = df['close'].pct_change(n).rolling(n).std()
+    df[factor_name] = (df['std']*df['MtmMean'])
+
+    return df
